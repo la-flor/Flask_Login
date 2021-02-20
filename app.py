9a7b166ce91afb9cf92b2d9d4b9a96a5dd38d808
lafBot <64456835+lafBot@users.noreply.github.com>
2021-02-20 13:27:19 -0700

    changes to raised errors, user_loader, and successful login_user

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import os
-from flask import Flask, request, render_template, redirect
+from flask import Flask, flash, request, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from forms import LoginForm, CreateUserForm
 
-from flask_login import LoginManager, login_required, login_user, current_user
-from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "you should have a password in your config file")
+db = SQLAlchemy()
 
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "you should have a password in your config file")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///flask_login')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -19,24 +19,18 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 toolbar = DebugToolbarExtension(app)
 
-
-db = SQLAlchemy()
 from models import connect_db, User
 connect_db(app)
 
 # instantiate and initialize login manager
 login_manager = LoginManager()
-login_manager.login_view = 'app.login'
 login_manager.init_app(app)
-
-# instantiate bcrypt
-bcrypt = Bcrypt()
 
 # connects flask-login users with database users
 @login_manager.user_loader
 def load_user(user_id):
     try:
-        user = User.query.get(user_id)
+        return User.query.get(user_id)
     except:
         return None
 
@@ -53,14 +47,14 @@ def create_user():
             user = User.create_user(form.username.data, form.password.data)
 
             if not user:
-                raise
+                raise AssertionError("Unable to insert user into database.")
             
-            # login_user(user)
+            login_user(user)
 
             return redirect('/')
         except:
-            print('Unable to create user.')
-            return redirect('/')
+            flash('Unable to create user.')
+            return redirect('/createuser')
     return render_template('create_user.html', form=form)
 
 
@@ -73,23 +67,21 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         try:
-            user = User.query.filter_by(username=form.username.data).first()
-            is_authorized = bcrypt.check_password_hash(user.password, form.password.data)
+            username = form.username.data
+            password = form.password.data
 
-            if not user and not is_authorized:
-                raise
-
+            user = User.is_valid(username=username, password=password)
             login_user(user)
-
+            
             flash('Logged in successfully.')
             return redirect('/admin')
         except:
-            print('Invalid username/password')
-            return redirect('/')
-    print(form.errors)
+            flash('Invalid username/password')
+            return redirect('/login')
+
     return render_template('login.html', form=form)
 
-@app.route('/admin', methods=['GET'])
+@app.route('/admin')
 @login_required
 def admin_page():
     return render_template('admin.html')
@@ -98,5 +90,5 @@ def admin_page():
 @login_required
 def logout():
     logout_user()
-    flask.flash('Logout successful.')
+    flash('Logout successful.')
     return redirect('/')
